a7565300b3958c73b8e6f7078b082bdc272e78d7
quandelaa <sergiotobamasilalahi@gmail.com>
2026-06-19 15:40:19 +0700

    batman

---
+++ src/elimination.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Elimination():
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def row_echelon_form(cls, matrix: list):
+        try:
+            A = np.array(matrix, float)
+        except Exception as e:
+            return e
+
+        rows, cols = A.shape
+        col_pivot = 0
+        
+        for i in range(rows):
+            next_row = i+1
+
+            if next_row > rows-1 or col_pivot > cols-1:
+                return A
+
+            if A[i][col_pivot] == 0: 
+                had_change = False
+
+                for piv in range(col_pivot, cols):
+                    for piv_ in range(i, rows):
+                        if A[piv_][col_pivot] != 0:
+                            A[[piv_, i]] = A[[i, piv_]]
+                            had_change = True
+                            break
+
+                    if A[i][piv] != 0:
+                        col_pivot = piv
+                        had_change = True
+                        break
+
+                if not had_change:
+                    return A
+
+            for j in range(next_row, rows):
+                multiplier = A[j][col_pivot] / A[i][col_pivot]
+
+                new_row = A[j] - (A[i] * multiplier)
+                A[j] = new_row
+
+            col_pivot += 1
