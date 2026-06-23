a4488441b0402c1436c0bcc3f6e229fb926dddd6
quandelaa <sergiotobamasilalahi@gmail.com>
2026-06-23 16:08:14 +0700

    chore: made tests for new rref() function

--- tests/elimination_tests.py
+++ tests/elimination_tests.py
@@ -1,5 +1,5 @@
 import numpy as np
-from gauss import ref
+from gauss import rref, ref
 
 def ref_test():
     A_matrices = [
@@ -95,5 +95,96 @@ def ref_test():
 
     return successes
 
+def rref_test():
+    A_matrices = [
+        [[0, 0, 0],
+         [0, 0, 0]],
+
+        [[1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1]],
+
+        [[0, 1],
+         [1, 0]],
+
+        [[1, 2, 3],
+         [4, 5, 6]],
+
+        [[2, 4],
+         [3, 6]],
+
+        [[1, 2, 3, 4],
+         [5, 6, 7, 8],
+         [9, 10, 11, 12]],
+
+        [[1, 2, 3],
+         [2, 4, 6]],
+
+        [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]],
+
+        [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9],
+         [10, 11, 12]],
+
+        [[1, 2, 3, 4, 5],
+         [6, 7, 8, 9, 10],
+         [11, 12, 13, 14, 15]]
+    ]
+
+    RREF_matrices = [
+        [[0, 0, 0],
+         [0, 0, 0]],
+
+        [[1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1]],
+
+        [[1, 0],
+         [0, 1]],
+
+        [[1, 0, -1],
+         [0, 1,  2]],
+
+        [[1, 2],
+         [0, 0]],
+
+        [[1, 0, -1, -2],
+         [0, 1,  2,  3],
+         [0, 0,  0,  0]],
+
+        [[1, 2, 3],
+         [0, 0, 0]],
+
+        [[1, 0, -1],
+         [0, 1,  2],
+         [0, 0,  0]],
+
+        [[1, 0, -1],
+         [0, 1,  2],
+         [0, 0,  0],
+         [0, 0,  0]],
+
+        [[1, 0, -1, -2, -3],
+         [0, 1,  2,  3,  4],
+         [0, 0,  0,  0,  0]]
+    ]
+
+    successes = 0
+
+    for (A, rref_expected) in zip(A_matrices, RREF_matrices):
+        rref_expected_np = np.array(rref_expected, dtype=float)
+        rref_computed = rref(A)
+
+        if np.allclose(rref_computed, rref_expected_np, atol=1e-12) is True:
+            successes += 1
+
+    return successes
+            
 def test_ref():
     assert ref_test() == 10
+
+def test_rref():
+    assert rref_test() == 10
