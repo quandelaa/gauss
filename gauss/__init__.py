7a55629362682bce423678a8d402f29b0c548285
quandelaa <sergiotobamasilalahi@gmail.com>
2026-06-26 11:49:17 +0700

    chore: add import compatibility to new inverse, gauss-jordan, utility functions and also new-made exceptions/errors

--- gauss/__init__.py
+++ gauss/__init__.py
@@ -4,8 +4,25 @@ Gauss - a linear algebra library
 
 from gauss.elimination import ref
 from gauss.elimination import rref
+from gauss.solvers import inverse
+from gauss.elimination import gauss_jordan
+from gauss import utils
+from gauss.exceptions import (
+        GaussError,
+        NotSquareError,
+        DimensionMismatchError,
+        NonInvertibleError,
+)
 
 __all__ = [
+    "NonInvertibleError",
+    "DimensionMismatchError",
+    "NotSquareError",
+    "GaussError",
+
+    "utils",
+    "gauss_jordan",
+    "inverse",
     "ref",
     "rref",
 ]
