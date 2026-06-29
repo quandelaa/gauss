40316c71d6ceebc09b927fcf9070215adb69fc83
quandelaa <sergiotobamasilalahi@gmail.com>
2026-06-29 18:03:14 +0700

    feat: add new function to check whether a matrix is a square matrix or not

--- gauss/utils.py
+++ gauss/utils.py
@@ -1,7 +1,7 @@
 from gauss import rref
 import numpy as np
 
-def is_invertible(matrix: np.ndarray | list[list[float]]):
+def is_invertible(matrix: np.ndarray | list[list[float]]) -> bool:
     """
     Returns whether a matrix is invertible or not
     """
@@ -17,7 +17,18 @@ def is_invertible(matrix: np.ndarray | list[list[float]]):
     I = np.array(raw_I)
 
     R = rref(A)
-    if np.allclose(R, I, atol=1e-12) is False:
-        return False
 
-    return True
+    return(np.allclose(R, I))
+
+def is_square(matrix: np.ndarray | list[list[float]]) -> bool:
+    """
+    Returns whether a matrix is square or not
+    """
+
+    A = np.array(matrix)
+    rows, cols = A.shape
+
+    if rows == cols:
+        return True
+
+    return False
